2351f72308dd4cfa477152d76e636558a4f07edb
Adam Edgett <AdamEdgett@gmail.com>
2018-09-27 19:02:05 -0400

    Add initial back end application

---
+++ app.rb
@@ -0,0 +1,63 @@
+require 'date'
+require 'json'
+require 'base64'
+require 'uri'
+require 'net/http'
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'pry'
+
+enable :cross_origin
+set :allow_origin, :any
+set :allow_methods, [:get]
+set :expose_headers, ['Content-Type']
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+get '/api/status' do
+  content_type :json
+  tweet = get_latest_tweet
+  status = get_status(tweet['text']).downcase
+  date = DateTime.parse(tweet['created_at'])
+  {
+    'openStatus' => status,
+    'dateTime' => date,
+    'tweet' => tweet['text'],
+    'tweetUrl' => "https://twitter.com/LoganToChelsea/status/#{tweet['id_str']}"
+  }.to_json
+end
+
+def get_status(tweet)
+  /Chelsea (st|st.|street) bridge (is )?(?<status>down|up)/i.match(tweet)['status']
+end
+
+def get_latest_tweet
+  uri = URI('https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&screen_name=logantochelsea')
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  headers = {
+    'Authorization' => "Bearer #{bearer_token}"
+  }
+  res = http.get(uri.request_uri, headers)
+  JSON.parse(res.body).first
+end
+
+def bearer_token
+  @bearer_token ||= get_new_bearer_token
+end
+
+def get_new_bearer_token
+  token_uri = URI('https://api.twitter.com/oauth2/token')
+  http = Net::HTTP.new(token_uri.host, token_uri.port)
+  http.use_ssl = true
+  auth_token = Base64.strict_encode64("#{ENV['TWITTER_API_KEY']}:#{ENV['TWITTER_API_SECRET']}")
+  headers = {
+    'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8',
+    'Authorization' => "Basic #{auth_token}"
+  }
+  data = 'grant_type=client_credentials'
+  res = http.post(token_uri.request_uri, data, headers)
+  JSON.parse(res.body)['access_token']
+end
